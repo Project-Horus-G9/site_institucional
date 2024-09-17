dfc99a83b06e0b0317a47762ed53b01fa64c2eb1
shift39 <marco.magalhaes@sptech.school>
2024-09-17 11:35:49 -0300

    Serviço de mensagem de token para login em duas etapas

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home"
 import Login from "./pages/Login/Login"
+import TokenLogin from "./pages/TokenLog/TokenLogin";
 import DashGeral from "./pages/DashGeral/DashGeral" 
 
 function Router() {
@@ -9,6 +10,7 @@ function Router() {
         <Routes>
             <Route path={"/"} element={<Home />}/>
             <Route path={"/login"} element={<Login />}/>
+            <Route path={"/tokenlogin"} element={<TokenLogin />}/>
             <Route path={"/dash-geral"} element={<DashGeral />}/>
         </Routes>
         </BrowserRouter>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,11 +2,38 @@ import React, { useState } from 'react';
 import { IconButton, Toolbar, Link, Paper, AppBar, Typography, Container, Menu, Grid, Button, Hidden, Box, Input, styled } from '@mui/material';
 import style from './Login.module.css';
 import { useNavigate } from 'react-router-dom';
-
+import emailjs from 'emailjs-com';
+import { v4 as uuidv4 } from 'uuid';
 
 function Login() {
 
     const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [token, setToken] = useState('');
+    const acessToken = uuidv4();
+
+    const loginToken = (event) => {
+        event.preventDefault();
+
+        const templateParams = {
+            to_email: email,
+            subject: 'Token de acesso',
+            message: `Seu token ${acessToken} `,
+        };
+
+        emailjs.send()
+            .then((response) => {
+                console.log('Email sent successfully:', response);
+                navigate('/tokenlogin', { state: { token: acessToken } });
+            })
+            .catch((error) => {
+                console.error('Error sending email:', error);
+            });
+
+            
+    }   
+
 
     return (
         <>
@@ -21,17 +48,23 @@ function Login() {
 
                                     </div>
                                     <div className={style.loginBoxBottomSide}>
-                                        <form action="" className={style.loginBoxForm}>
-                                        <span className={style.spanLogin}>
-                                        <div className={style.div1}></div>
-                                        <input type="text" placeholder='Your Email'/>
-                                        </span>
-                                        <span className={style.spanLogin}>
-                                        <div className={style.div2}></div>
-                                        <input type="text" placeholder='Your Email'/>
-                                        </span>
-                                        <b>Forgot your password?</b>
-                                        <Button onClick={() => navigate('/dash-geral')}>Login</Button>
+                                        <form onSubmit={loginToken} className={style.loginBoxForm}>
+
+                                            <span className={style.spanLogin}>
+                                                <div className={style.div1}></div>
+                                                <input type="text" placeholder='Your Email' id="email" value={email}
+                                                onChange={(e) => setEmail(e.target.value)}/>
+                                            </span>
+
+                                            <span className={style.spanLogin}>
+                                                <div className={style.div2}></div>
+                                                <input type="password" placeholder='Your Password' id="senha"   value={senha}
+                                                onChange={(e) => setSenha(e.target.value)}/>
+                                            </span>
+
+                                            <b>Forgot your password?</b>
+
+                                            <Button type="submit">Login</Button>
                                         </form>
                                     </div>
                                 </div>
